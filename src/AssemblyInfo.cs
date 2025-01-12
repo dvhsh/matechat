0fab39dd8c8861d1e1067635bc3fc686aec44738
David <175678329+dvhsh@users.noreply.github.com>
2025-01-12 00:01:44 -0800

    Update AssemblyInfo.cs

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 using MelonLoader;
 
-[assembly: MelonInfo(typeof(matechat.Core), "MateChat", "1.0.1", "dvhsh", null)]
+[assembly: MelonInfo(typeof(matechat.Core), "MateChat", "1.0.2", "dvhsh", null)]
 [assembly: MelonGame("infiniteloop", "DesktopMate")]
