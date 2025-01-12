fc7f2a0a68bc2458c1314594afa24b6203f1be63
David <175678329+dvhsh@users.noreply.github.com>
2025-01-11 21:05:56 -0800

    Create JsonUtil.cs

---
+++ src/util/JsonUtil.cs
@@ -0,0 +1,16 @@
+namespace matechat.util
+{
+    public static class JsonUtil
+    {
+        public static string EscapeJsonString(string str)
+        {
+            if (string.IsNullOrEmpty(str)) return str;
+            return str.Replace("\"", "\\\"")
+                     .Replace("\n", "\\n")
+                     .Replace("\r", "\\r")
+                     .Replace("\t", "\\t")
+                     .Replace("\\", "\\\\");
+        }
+
+    }
+}
