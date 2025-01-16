e9e2369014b9ba9fdc7b0be0d7bb435d3a4ae660
David <175678329+dvhsh@users.noreply.github.com>
2025-01-16 15:42:59 -0800

    [chore] bump version

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 using MelonLoader;
 
-[assembly: MelonInfo(typeof(matechat.Core), "MateChat", "1.0.3", "dvhsh", null)]
+[assembly: MelonInfo(typeof(matechat.Core), "MateChat", "1.0.3.1", "dvhsh", null)]
 [assembly: MelonGame("infiniteloop", "DesktopMate")]
