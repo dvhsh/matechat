a5c4cc011a7630f7e4bf3fbfa3f46a15a87f3d78
David <175678329+dvhsh@users.noreply.github.com>
2025-01-19 11:58:02 -0800

    [chore] fmt

--- src/util/JsonUtil.cs
+++ src/util/JsonUtil.cs
@@ -4,13 +4,13 @@ namespace matechat.util
     {
         public static string EscapeJsonString(string str)
         {
-            if (string.IsNullOrEmpty(str)) return str;
+            if (string.IsNullOrEmpty(str))
+                return str;
             return str.Replace("\"", "\\\"")
-                     .Replace("\n", "\\n")
-                     .Replace("\r", "\\r")
-                     .Replace("\t", "\\t")
-                     .Replace("\\", "\\\\");
+                .Replace("\n", "\\n")
+                .Replace("\r", "\\r")
+                .Replace("\t", "\\t")
+                .Replace("\\", "\\\\");
         }
-
     }
 }
