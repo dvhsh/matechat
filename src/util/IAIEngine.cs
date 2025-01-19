4f4d4ed54686bdec2497919c8279afeac2e83f21
David <175678329+dvhsh@users.noreply.github.com>
2025-01-19 11:57:30 -0800

    Update IAIEngine.cs

--- src/util/IAIEngine.cs
+++ src/util/IAIEngine.cs
@@ -1,14 +1,11 @@
-﻿using System;
 using System.Collections;
 
 namespace matechat.util
 {
-    namespace matechat.util
+    public interface IAIEngine
     {
-        public interface IAIEngine
-        {
-            IEnumerator SendRequest(string userMessage, string systemPrompt, System.Action<string, string> callback);
-            IEnumerator TestEngine(System.Action<bool, string> callback);
-        }
+        IEnumerator SendRequest(string userMessage, string systemPrompt,
+                                System.Action<string, string> callback);
+        IEnumerator TestEngine(System.Action<bool, string> callback);
     }
 }
