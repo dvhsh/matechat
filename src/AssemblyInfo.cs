d8dbc836aed0a8d98605bb2c61b575b5a54a3137
MidgetBrony <russell@hempton.us>
2025-01-23 21:08:08 +0100

    Fixed Generation of Constants File, If changes need to be made overwrite the Constants.cs.bak

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -1,3 +1,6 @@
 using MelonLoader;
-[assembly: MelonInfo(typeof(matechat.Core), "MateChat", "1.1.1", "dvhsh", null)]
+using matechat;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(matechat.Core), "MateChat", Constants.VERSION, "dvhsh", null)]
 [assembly: MelonGame("infiniteloop", "DesktopMate")]
